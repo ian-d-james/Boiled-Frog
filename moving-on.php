cef71689060a3778f191d221ce7bd24e541e60b2
ian-d-james <ian.d.james@gmail.com>
2019-03-06 17:08:06 -0800

    Update

--- moving-on.php
+++ moving-on.php
@@ -67,27 +67,64 @@
 							   As the water gradually heats up, the frog will sink into a tranquil stupor, exactly like one of us in a hot bath, 
 							   and before long, with a smile on its face, it will unresistingly allow itself to be boiled to death."</p>
 
-							   <div style="float:right; margin-left:20px;">
-									<iframe width="560" height="315" src="https://www.youtube.com/embed/LnnDOMyZjbE?rel=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							   <div style="float:right; margin-top:-10px; margin-left:30px;">
+									<img src="../images/studies_consensus.jpg" />
 							</div> 
 
 							<p>Historically, climate change, has been largely characterized as a slow and incremental process. We might be excused, therefore, for ignoring or
-								disputing climatic changes such as temperature and CO2 elevations over the past 250 years. What is inexcusable, however, is choosing to disregard 
+								disputing climatic changes such as temperature and CO2 elevations over the past 250 years. Can we afford, however, to disregard 
 								the <a target="_blank" href="https://www.skepticalscience.com/global-warming-scientific-consensus-intermediate.htm">virtual consensus</a> of 
-								our subject matter experts (climate scientists).</p>
+								our subject matter experts (climate scientists)?</p>
 
 							<p>The international scientific consensus that humans are causing recent global warming is shared by <b>90% – 100%</b> of publishing climate scientists according to 
 							   <a target="_blank" href="https://www.researchgate.net/publication/301247690_Consensus_on_Consensus_A_Synthesis_of_Consensus_Estimates_on_Human-Caused_Global_Warming">six independent studies</a>.
 							   This is consistent with the <b>97%</b> consensus reported by <a target="_blank" href="http://iopscience.iop.org/article/10.1088/1748-9326/8/2/024024?fbclid=IwAR1-jplIFV6QcKtfeO5N-4upDVqP_TDl5bw0YQyLZd36tASoPsI-yOh4buM">Cook et al</a> based on 11,944 
 							   abstracts of research papers, of which 4,014 took a position on the cause of recent global warming.
-							<div>Download the detailed 2013 and 2016 climate change consensus papers and supplementary materials, Cook et al, in PDF format below:<br /><a style="margin-right:50px;" target="_blank" href="../assets/content/climate-change-consensus.pdf">
+							<div>You can download the detailed 2013 and 2016 climate change consensus papers and supplementary materials, Cook et al, in PDF format below:<br /><a style="margin-right:50px;" target="_blank" href="../assets/content/climate-change-consensus.pdf">
 								<img style=" margin-top:20px;" src="../images/pdf.png"></a><a style="margin-right:50px;" target="_blank" href="../assets/content/Article.pdf"><img src="../images/pdf.png"></a>
 								<a style="margin-right:50px;" target="_blank" href="../assets/content/Supplementary Information.pdf"><img src="../images/pdf.png"></a><a target="_blank" href="../assets/content/Data.pdf"><img src="../images/pdf.png"></a>
 							</div></p>
-							<p>Choosing to dismiss the shared conclusions and recommendations of virtually <a target="_blank" href="https://climate.nasa.gov/scientific-consensus/">
-							   all scientific professionals and organizations</a> constitutes wilful ignorance and, arguably, civil and criminal negligence.</p>
 
-							<p>This site does not reiterate arguments for man-made climate change. There are many sites (see landing page) and <a href="references.php">references</a> providing compelling scientific evidence and 
+							<p>But is <a target="_blank" href="https://climate.nasa.gov/scientific-consensus/">consensus</a> a good basis for scientific decision making? A strong skeptic of global warming, author Michael Crichton offered the following opinion taken from his series 
+							   of environmental and global warming <a target="_blank" href="../assets/content/crichton_3.pdf">essays</a> written in 2009:</p>
+
+							<div style="font-style: italic; margin-left:30px; margin-right:30px;">"Let's be clear: the work of science has nothing whatever to do with consensus. Consensus is the business of politics.  Science, on the contrary, requires only one investigator who happens 
+							to be right, which means that he or she has results that are 
+							verifiable by reference to the real world.  In science, consensus is irrelevant. What is relevant is <b>reproducible results</b>. The greatest scientists in history are great precisely because they broke with the consensus."</div><br />
+
+							<p>If consensus is irrelevant, what should be the foundation for our decision making? To answer this let's revisit the scientific method:</p>
+							
+							<ol style="margin-left:30px;">
+								<li>Make an observation or observations.</li>
+								<li>Ask questions about the observations and gather information.</li>
+								<li>Form a hypothesis — a tentative description of what's been observed, and make <b>predictions based on that hypothesis</b>.</li>
+								<li>Test the hypothesis and predictions in an experiment that can be <b>reproduced</b>.</li>
+								<li>Analyze the data and draw conclusions; accept or reject the hypothesis or modify the hypothesis if necessary.</li>
+								<li>Reproduce the experiment until there are no discrepancies between observations and theory.</li>
+							</ol>
+
+							<p><b>Predictability</b> is the ability to accurately define trends and future conditions such as atmospheric and oceanic temperature. Climate <a target="_blank" href="https://link.springer.com/article/10.1007/s00382-017-3939-8#Sec2">models</a> are inherently uncertain given that initial conditions can never be known perfectly and that the model is an imperfect representation 
+							of global climate and its underlying physics. So just how accurate are the climate model predictions that are the foundation of the scientific publications above? Climate models published since 1973 have generally been quite skillful in projecting future warming. An overview of climate model predictive accuracy (both past and future) is provided on the 
+							<a target="_blank" href="https://skepticalscience.com/how-well-have-models-predicted-gw.html">Skeptical Science</a> website. While some 
+							temperature predictions were too low and some too high, they all show outcomes reasonably close to what has actually occurred.</p>
+
+							<p><b>Reproducibility</b> is the ability to test a result using independent (different) methods and data processing. This term should not be confused with <b>replicability</b> which is the ability to rerun and validate the analysis using the same methods and data processing.</p>
+
+							<div style="float:right;"><iframe style="margin-left:30px; margin-bottom:10px;" src="https://embed.ted.com/talks/gavin_schmidt_the_emergent_patterns_of_climate_change" width="500" height="280" frameborder="0" scrolling="no" ></iframe></div>
+							
+							<p>Climate model <a target="_blank" href="https://psmag.com/environment/what-a-reproducibility-crisis-committee-found-when-it-looked-at-climate-science">variability</a> isn't surprising given the enormous number of variables and interdependencies. Traditional reductionism can't be applied to climate modelling (see Gavin Schmidt's Ted talk), instead holistic climate models must contend with fourteen orders of 
+							spatial and time magnitude. Models require massive and ever growing data sets 
+							that can limit reproducibility. Even with good <a target="_blank" href="http://www.realclimate.org/index.php/archives/2018/05/transparency-in-climate-science/">data transparency</a>, resource and tool constraints (e.g., bandwidth and computational restrictions) 
+							may preclude or hinder reproducing and verifying model results. Fortunately, there are a number of initiatives to enable climate scientists to better share, compare and analyze climate model results. One such initiative is the the 
+							<a target="_blank" href="https://www.wcrp-climate.org/wgcm-cmip">coupled intercomparison project (CMIP)</a>.</p>
+
+							<p>Climate model <a target="_blank" href="https://eos.org/opinions/climate-models-are-uncertain-but-we-can-do-something-about-it">predictive certainty</a> will continue to improve, although most climate scientists acknowledge that there are predictive limits. No matter how sophisticated our models become, there will always be an irreducible element of chaos in the earth’s climate system that no supercomputer can eliminate.</p>
+
+							<p>There is much more evidence of global warming, of course, than that predicted by climate models. Examples include <a target="_blank" href="https://data.giss.nasa.gov/gistemp/">GISS Surface Temperature Analysis</a> 
+							 which is the ongoing tracking of global surface temperature changes (both replicable and reproducible) and paleo-climate records such as those collected in projects like 
+							 <a target="_blank" href="http://www.pages-igbp.org/ini/wg/2k-network/intro">PAGES2K</a>.</p>
+
+							<p>This site does not attempt to address the full body of evidence for man-made climate change. There are many sites (see landing page) and <a href="references.php">references</a> providing compelling scientific evidence and 
 							   expert testimony that man is driving and accelerating climate change. Instead, this site moves beyond the argument and targets individuals and organizations 
 							   seeking strategies and actions to hold our educators, politicians, corporations and governments accountable for current and future climate policy. To hold
 							   today's decision-makers accountable for the growing climate consequences that will be borne by our children and grandchildren. 
