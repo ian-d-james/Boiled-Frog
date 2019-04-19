10b1238cc4f26ddaf9537afb170ac9b98bf592f6
ian-d-james <ian.d.james@gmail.com>
2019-04-19 12:37:57 -0700

    Update

---
+++ legal-remedies.php
@@ -0,0 +1,135 @@
+<!DOCTYPE HTML>
+<!--
+	Boiled Frog - Climate Change Awareness and Strategy
+	Ian D James
+-->
+<html>
+	<head>
+ 		<title>Boiled Frog - Climate Change, Civil and Criminal Negligence</title>
+		<meta name="description" content="Climate Change Criminality">
+		<meta name="author" content="Ian James">
+		<meta charset="utf-8" />
+		<meta name="keywords" content="environment, climate, change, global, warming, greenhouse, enhanced, effect, accountability, criminality, cuplability, IPCC, NASA">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58191121-2"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-58191121-2');
+		</script>
+
+	</head>
+
+	<body class="subpage">
+
+		<!-- Header -->
+		<header id="header">
+				<div class="logo"><a href="http://boiledfrog.ca">Boiled Frog</a></div>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+		<?php include 'navigation.html';?>
+
+		<!-- One -->
+			<section id="One" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Government and Corporate Negligence - A Failure to Act</p>
+						<h2>Legal Remedies</h2>
+					</header>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style2">
+				<div class="inner">
+					<div class="box">
+						<div class="content">
+							<header class="align-center">
+								<p>Negligence</p>
+								<h3>A breach of a duty of care causing harm by <b>failing to act</b>.</h3>
+							</header>
+							<br />
+							<p>
+							<img src="../images/lawsuit.jpg" style="float:left; margin-right:20px; margin-bottom:10px;">
+							Unlike the <a target="_blank" href="https://treaties.un.org/Pages/ViewDetails.aspx?src=IND&mtdsg_no=XXVII-7-a&chapter=27&clang=_en">Kyoto Protocol</a>, which set legally-binding targets, 
+							the <a target="_blank" href="https://treaties.un.org/Pages/ViewDetails.aspx?src=IND&mtdsg_no=XXVII-7-d&chapter=27&clang=_en">Paris Agreement</a> allows for voluntary and nationally determined targets. Regional climate goals are politically encouraged rather than legally bound. 	
+							Government action to meet these commitments has changed public expectations for corporate action, investor diligence on climate change risk, and the role of state and local authorities.	
+							As a result, tribunals and judges are increasingly being asked to deal with arguments and facts related to climate change negligence, i.e., a breach of a duty of care causing harm by failing to act. Governments and companies 
+							must exercise reasonable care in their actions, by taking account of the potential harm that they might foreseeably cause to people or property.</p>
+							<p>Climate change litigation describes a range of legal proceedings related to climate change. It can be directed at public and private companies, federal governments, city administrations and insurance companies.</p>
+							
+							<ul style="padding-left:0px;">Climate change litigation has two broad categories:
+								<li style="margin-left:30px;">Public law actions against governments and public authorities, raising human rights, constitutional and administrative law arguments</li>
+								<li style="margin-left:30px;">Private law actions based in areas of law such as tort, fraud, planning and company law</li>
+							</ul>
+							<p>According to <a target="_blank" href="https://www.whitecase.com/">White & Case</a>, an international law firm based in New York City, almost 1,000 climate change-related cases have been filed to date around the world, covering 25 countries. Overall, corporations and industrials 
+							are the most common claimants or plaintiffs in these cases, with governments being the most common defendants. In the United States, claims related to duty of care or failure to warn are analogous to the scope of past litigation 
+							over tobacco or asbestos.</p>
+							<h3>Key drivers for climate change litigation</h3>
+
+							<ul style="padding-left:0px;">White & Case has identified five drivers for new and ongoing climate change litigation:
+								<li style="margin-left:30px;">Compensation for the costs of adaptation to climate change</li>
+								<li style="margin-left:30px;">Preventing future emissions and contributions to climate change</li>
+								<li style="margin-left:30px;">Requiring governments or regulators to take action to meet national or international commitments</li>
+								<li style="margin-left:30px;">Raising awareness and exerting pressure on corporate actors, regulators or investors</li>
+							</ul>
+
+							<h3>Constitutional law and human rights claims</h3>
+							<p style="text-align: center; font-style:italic;">“Exercising my ‘reasoned judgment,’ I have no doubt that the right to a climate system capable of sustaining<br/>human life is fundamental to a free and ordered society.”<br/>
+							- U.S. District Judge Ann Aiken</p>
+
+							<p>In recent years, climate change cases have been filed by ordinary citizens all over the world, demonstrating the power of holding governments accountable in court</p>
+							<p>
+							<img src="../images/netherlands-lawsuit.jpg" style="float:left; margin-right:20px; margin-bottom:10px;">
+							The 2015 <a target="_blank" href="https://www.elaw.org/nl.urgenda.15">Urgenda Foundation v. The State of the Netherlands</a> climate case against the Dutch Government was the first in the world in which 
+							citizens held their government accountable for contributing to dangerous climate change.</p>
+							<p>On 24 June 2015, the District Court of The Hague ruled the government must cut its greenhouse gas emissions by at least 25% by the end of 2020 (compared to 1990 levels). The ruling required the government to immediately 
+								take more effective action on climate change. The case, which was brought on behalf of 886 Dutch citizens, made climate change a major political and social issue in the Netherlands and transformed domestic climate change policy. 
+							</p>
+							<p>For more details, visit this <a target="_blank" href="https://www.urgenda.nl/en/themas/climate-case/">website</a>.</p>
+
+							<br />
+
+							<p>
+							<img src="../images/usa-lawsuit.jpg" style="float:left; margin-right:20px; margin-bottom:10px;">
+							The 2015 <a target="_blank" href="https://www.ourchildrenstrust.org/juliana-v-us">Juliana v. United States</a> climate case against the U.S. Government was filed in the U.S. District Court for the District of Oregon.
+							The claim asserts that, through the government's affirmative actions that cause climate change, it has violated the youngest generation’s constitutional rights to life, liberty, and property, as well as failed to protect essential public trust resources.</p>
+							<p>On March 1, 2019 amicus curiae (“friend of the court”) briefs were filed with the Ninth Circuit Court of Appeals. In all, 15 amicus briefs were filed, including members of U.S. Congress, legal scholars, religious and women’s groups, businesses, historians, 
+								medical doctors, international lawyers, environmentalists, and more than 32,000 youth under the age of 25.</p>
+							<p>For more details, visit this <a target="_blank" href="https://www.ourchildrenstrust.org/juliana-v-us">website</a>.</p>
+
+							<br />
+
+							<p>
+							<img src="../images/new-zealand-lawsuit.jpg" style="float:left; margin-right:20px; margin-bottom:10px;">
+							The 2017 <a target="_blank" href="https://minterellison.co.nz/our-view/thomson-v-minister-for-climate-change-issues-the-expanding-reach-of-judicial-review">Thomson v Minister for Climate Change Issues</a> case against the 
+							the New Zealand government and their former climate change minister claimed that they failed to review climate targets for 2050 following the release of the Fifth Assessment Report (AR5) by the UN's Inter-governmental Panel on Climate Change (IPCC).
+							</p>
+							<p>While dismissed, the decision of the High Court serves as an important reminder to governments that Courts have broad powers of 
+								review and are able to hold governments and their ministers to account when making important decisions on climate change.</p>
+							<p>For more details, visit this <a target="_blank" href="https://minterellison.co.nz/about">website</a>.</p>
+
+							<br />
+
+							<p>The <a target="_blank" href="http://columbiaclimatelaw.com/">Sabin Center for Climate Change Law</a> at Columbia Law School and the firm <a target="_blank" href="https://www.arnoldporter.com/en/perspectives/news">Arnold & Porter</a> provide a searchable database of ongoing <a target="_blank" href="http://climatecasechart.com/non-us-climate-change-litigation/">internationa</a> and <a target="_blank" href="http://climatecasechart.com/us-climate-change-litigation/">U.S.</a> climate change legal actions are outlined on the Climate Case Chart website. 
+							The international climate litigation data base was created in 2011 and is updated regularly. It currently includes 281 cases, with links to 301 case documents. The U.S. chart is updated on a monthly basis, and currently includes 1013 cases* with links to 4608 case documents.</p>
+
+							<p><a target="_blank" href="https://www.whitecase.com/">White & Case</a> provide an excellent overview of climate change litigation available as a PDF that can be downloaded <a target="_blank" href="../assets/content/Global climate change litigation and laws.pdf">here</a>.</p>
+
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+		<?php include 'footer.html';?>
+
+	</body>
+</html>
